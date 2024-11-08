62b0535cbde0983fa093aa5ed157ecb9a1e8ccc0
Pavlo <oleynik.pavel.97@gmail.com>
2024-11-08 15:22:24 +0200

    Fix for eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,19 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 
-
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
-  {languageOptions: { globals: globals.browser }},
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      sourceType: "commonjs",
+      ecmaVersion: "latest",
+      globals: {
+        ...globals.browser,
+        ...globals.node,    // add env Node.js
+        ...globals.jest,    // add env Jest
+      },
+    },
+  },
   pluginJs.configs.recommended,
 ];
